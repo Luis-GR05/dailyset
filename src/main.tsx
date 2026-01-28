35edf931f2152927373062431dd9f04d02f86654
LuisGR <lgordillor01@iesalbarregas.es>
2026-01-28 18:03:45 +0100

    instalar tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Logo from './componentes/Logo'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Logo />
   </StrictMode>,
 )
