7d160b0518b9b07813a9eb6acf60d22c5d0a9bcf
LuisGR <lgordillor01@iesalbarregas.es>
2026-01-28 18:27:56 +0100

    Medio hacer barra lateral

--- src/componentes/BarraLateral.tsx
+++ src/componentes/BarraLateral.tsx
@@ -1,7 +1,14 @@
+import Logo from "./Logo";
+
 const BarraLateral = () => {
     return (
-        <section>
-            
+        <section className="bg-black-500 flex">
+            <Logo />
+            <a href="#" className="">Rutinas</a>
+            <a href="#">Historial</a>
+            <a href="#">Ejercicios</a>
+            <a href="#">Estadísticas</a>
+            <a href="#">Perfil</a>
         </section>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Logo from './componentes/Logo'
+import BarraLateral from './componentes/BarraLateral'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Logo />
+    <BarraLateral />
   </StrictMode>,
 )
