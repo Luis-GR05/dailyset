32f1c48342ab69bc6cfdb179f98bf1f650acf3b6
chocarro <chocarroo@gmail.com>
2026-02-09 17:13:36 +0100

    login

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-import BarraLateral from './componentes/BarraLateral'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BarraLateral />
-=======
+// Importamos la página de Login  tu carpeta de páginas
 import { Login } from './paginas/Login'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Login />
->>>>>>> 9e3abd4cde766766c1e3c04d4dcfb76e5a0ab021
   </StrictMode>,
 )
